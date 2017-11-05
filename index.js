433fe1003ab531707bf490740f004bd3cad1df8c
buckethead1986 <rspeckpfs@gmail.com>
2017-11-05 17:24:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+function Rectangle(sides, width, height) {
+  this.sides = sides;
+  this.width = width;
+  this.height = height;
+  this.area = function() {
+    return this.width * this.height;
+  };
+  this.perimeter = function() {
+    return (this.width + this.height) * 2;
+  };
+}
+
+// Square constructor
+function Square(sides, length) {
+  Rectangle.call(this, sides, length, length);
+}
